97c305f4b738f276668922999bcdca855ac89089
Gabriel Undan <gundan@hawaii.edu>
2019-11-20 21:54:52 -1000

    Revert to 27d01a817880fba8145bd3cd564bfb4d84be34d8

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px', color: 'white' };
+    const divStyle = { paddingTop: '15px' };
     return (
         <footer>
           <div style={divStyle} className="ui center aligned container">
             <hr />
-              Books for Bows <br />
+              Department of Information and Computer Sciences <br />
               University of Hawaii<br />
               Honolulu, HI 96822 <br />
-            <a href="https://books-for-bows.github.io/">https://books-for-bows.github.io/</a>
+            <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
           </div>
         </footer>
     );

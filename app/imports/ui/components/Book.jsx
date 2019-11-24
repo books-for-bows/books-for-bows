e25328fd15ede585f2e7741f0bedaeca4f1261dc
lukemcd9 <lukemcd97@gmail.com>
2019-11-24 01:53:40 -1000

    Finished Book Card

---
+++ app/imports/ui/components/Book.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image, Label } from 'semantic-ui-react';
+
+export default class Book extends React.Component {
+
+  render() {
+    return (
+      <Card>
+        <Image src={ this.props.book.image }/>
+        <Card.Content>
+          <Card.Header>{ this.props.book.title }</Card.Header>
+          <Card.Meta>
+            { this.props.book.author }
+            <br/>
+            ISBN: { this.props.book.ISBN }
+          </Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
+          <Label color='blue'>{ this.props.book.course }</Label>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+Book.propTypes = {
+  book: PropTypes.object.isRequired
+};

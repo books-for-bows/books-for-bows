e92fb7bd66bdb5c70d3d5aed049de563a63c471b
Gabriel Undan <gundan@hawaii.edu>
2019-12-04 20:06:15 -1000

    Edited to init default data into collections Books and Listings

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,18 +1,25 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Books } from '../../api/books/Books';
+import { Listings } from '../../api/listings/Listings';
 
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
-function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.insert(data);
+function addData(collection, data) {
+  collection.insert(data);
 }
 
 /** Initialize the collection if empty. */
-if (Stuffs.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
+if (Books.find().count() === 0) {
+  if (Meteor.settings.defaultBook) {
+    console.log('Creating default book data.');
+    Meteor.settings.defaultData.map(data => addData(Books, data));
+  }
+}
+
+if (Listings.find().count() === 0) {
+  if (Meteor.settings.defaultListing) {
+    console.log('Creating default listing data.');
+    Meteor.settings.defaultData.map(data => addData(Listings, data));
   }
 }

66afb8441096c65ec33a30382ce9345e23c517df
Gabriel Undan <gundan@hawaii.edu>
2019-12-11 20:30:03 -1000

    Changed Shelf page table

--- app/imports/ui/components/ListingItem.jsx
+++ app/imports/ui/components/ListingItem.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class ListingItem extends React.Component {
   render() {
     return (
         <Table.Row>
-          <Table.Cell>{this.props.listing.price}</Table.Cell>
-          <Table.Cell>{this.props.listing.binding}</Table.Cell>
-          <Table.Cell>{this.props.listing.seller}</Table.Cell>
+          <Table.Cell singleLine>{this.props.listing.price}</Table.Cell>
+          <Table.Cell singleLine>{this.props.listing.binding}</Table.Cell>
+          <Table.Cell singleLine>{this.props.listing.seller}</Table.Cell>
+          <Table.Cell >{this.props.listing.description}</Table.Cell>
         </Table.Row>
     );
   }

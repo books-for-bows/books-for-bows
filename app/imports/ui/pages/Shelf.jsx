d087e7e5b372d6eb956bede78861415fe7ea0891
Gabriel Undan <gundan@hawaii.edu>
2019-12-14 20:25:37 -1000

    Fixed ESLint error

--- app/imports/ui/pages/Shelf.jsx
+++ app/imports/ui/pages/Shelf.jsx
@@ -42,9 +42,8 @@ class Shelf extends React.Component {
                   </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                {Listings.find({ ISBN: this.props.book_isbn }).fetch().map(
-                    (listing, index) => <ListingItem key={index} listing={listing} />
-                    )}
+                {Listings.find({ ISBN: this.props.book_isbn }).fetch()
+                    .map((listing, index) => <ListingItem key={index} listing={listing} />)}
                 </Table.Body>
                 </Table>
               </Grid.Column>

87e5f65abea3388857ac16317511c8ddcbc415f4
Nainoa Jorg <51459240+nainoan@users.noreply.github.com>
2019-12-14 18:45:58 -1000

    The Search Engine Doesn't Type

--- app/imports/ui/pages/Marketplace.jsx
+++ app/imports/ui/pages/Marketplace.jsx
@@ -1,45 +1,98 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Loader, Card, Search, Grid } from 'semantic-ui-react';
+import { Container, Card, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { HTTP } from 'meteor/http';
 import { _ } from 'meteor/underscore';
-import { Books } from '../../api/books/Books.js';
 import { Listings } from '../../api/listings/Listings.js';
 import Book from '../components/Book.jsx';
 
-/**Fashion a Search Engine that searches through books based on the ISBN */
+/** Renders the Profile Collection as a set of Cards. */
 class Marketplace extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state.isbn = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  state = {
+    books: [],
+    books_ready: false,
+    isbn: String,
+  };
+
+  handleChange(event) {
+    this.setState({ value: event.target.isbn.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  getBooks() {
+    if (!this.state.books_ready) {
+      const url = 'https://www.googleapis.com/books/v1/volumes';
+      const listings = Listings.find({}).fetch();
+      _.each(_.uniq(_.pluck(listings, 'ISBN')), (book) => {
+        HTTP.get(
+            url,
+            {
+              params: {
+                q: `isbn:${book}`,
+                // key: Meteor.settings.public.api_key,
+              },
+            },
+            (error, result) => {
+              if (!error) {
+                this.state.books.push(result.data.items[0].volumeInfo);
+                if (this.state.books.length === listings.length) {
+                  this.setState({ books_ready: true });
+                }
+              }
+            },
+        );
+      });
+    }
+  }
+
   /** Render the page once subscriptions have been received. */
   render() {
     return (
-        <Grid centered container>
-        <Search
-          fluid
-          placeholder='ISBN'
-          />
-        </Grid>
-        // <Container>
-        //   <Card.Group>
-        //     {_.map(this.Books.title, (book) => <Book key={book.isbn} book={book}/>)}
-        //   </Card.Group>
-        // </Container>
+        <form onSubmit={this.handleSubmit} centered>
+          <label>
+            ISBN:
+            <input type="text" value={this.state.isbn.value} onChange={this.handleChange}/>
+          </label>
+          <input type="submit" value="Submit"/>
+        </form>
+    );
+  }
+
+  renderPage() {
+    this.getBooks();
+    return (
+        <Container>
+          <Card.Group>
+            {this.state.books_ready ?
+                _.map(this.state.books, (book, index) => <Book key={index} book={book}/>) : ''}
+          </Card.Group>
+        </Container>
     );
   }
 }
 
 Marketplace.propTypes = {
   ready: PropTypes.bool.isRequired,
-  books: PropTypes.object.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Ensure that minimongo is populated with all collections prior to running render().
-  const sub1 = Meteor.subscribe('Books');
-  const sub2 = Meteor.subscribe('Listings');
+  const sub = Meteor.subscribe('Listings');
   return {
-    ready: sub1.ready() && sub2.ready(),
-    books: Books.find({}).fetch(),
+    ready: sub.ready(),
   };
 })(Marketplace);

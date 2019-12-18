851d3bb394d2f876ce36fba476963d943d5708a3
Gabriel Undan <gundan@hawaii.edu>
2019-12-17 23:00:03 -1000

    Removed unnecessary things

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,16 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import { Books } from '../../api/books/Books.js';
 import { Listings } from '../../api/listings/Listings';
 import { CourseBooks } from '../../api/coursebooks/CourseBooks';
 
 /* eslint-disable no-console */
 const coursebooksJson = JSON.parse(Assets.getText('coursebooks.json'));
 /** Initialize the database with a default data document. */
-
-function addBook(data) {
-  console.log(`  Adding: ${data.title}`);
-  Books.insert(data);
-}
 
 function addListing(data) {
   console.log(`  Adding: ${data.title}`);
@@ -20,14 +14,6 @@ function addListing(data) {
 function addCourseBook(data) {
   console.log(`  Adding: ${data.isbn}`);
   CourseBooks.insert(data);
-}
-
-/** Initialize the collection if empty. */
-if (Books.find().count() === 0) {
-  if (Meteor.settings.defaultBook) {
-    console.log('Creating default books.');
-    Meteor.settings.defaultBook.map(data => addBook(data));
-  }
 }
 
 if (Listings.find().count() === 0) {
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,28 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
-import { Books } from '../../api/books/Books';
 import { Listings } from '../../api/listings/Listings';
 import { CourseBooks } from '../../api/coursebooks/CourseBooks';
 
-Meteor.publish('Stuff', function publish() {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.find({ owner: username });
-  }
-  return this.ready();
-});
-
-/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
-Meteor.publish('StuffAdmin', function publish() {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.find();
-  }
-  return this.ready();
-});
-
 Meteor.publish('CourseBooks', () => CourseBooks.find());
-
-Meteor.publish('Books', () => Books.find());
 
 Meteor.publish('Listings', () => Listings.find());
